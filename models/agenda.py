aaec8dccad73578c866e393eb2a4e254877ef5a1
unknown <henrique.ph935@gmail.com>
2023-03-10 09:54:46 -0300

    Cria autenticacao de user

--- models/agenda.py
+++ models/agenda.py
@@ -1,4 +1,16 @@
-class AgendaModel:
+from sql_alchemy import banco
+
+class AgendaModel(banco.Model):
+
+    __tablename__ = 'agenda'
+
+    agenda_id = banco.Column(banco.Integer, primary_key=True)
+    nome = banco.Column(banco.String(50), nullable=False)
+    numero = banco.Column(banco.String(15), nullable=False)
+    email = banco.Column(banco.String(50), nullable=False)
+    cidade = banco.Column(banco.String(50), nullable=False)
+
+
     def __init__(self, agenda_id, nome, numero, email, cidade):
         self.agenda_id = agenda_id
         self.nome = nome
@@ -14,3 +26,36 @@ class AgendaModel:
             'email': self.email,
             'cidade':self.cidade,
         }
+    
+    @classmethod
+    def find_agenda(cls, agenda_id):
+        agenda = cls.query.filter_by(agenda_id=agenda_id).first() #SELECT * FROM agenda WHERE agenda_id = agenda_id
+        if agenda:
+            return agenda
+        return None
+
+
+    def save_agenda(self):
+        banco.session.add(self)
+        banco.session.commit()
+
+
+    def update_contato(self, nome, numero, email, cidade):
+        self.nome = nome
+        self.numero = numero
+        self.email = email
+        self.cidade = cidade
+
+
+
+    def delete_contato(self):
+        banco.session.delete(self)
+        banco.session.commit()
+        
+
+
+    #def find_agenda(agenda_id):
+     #   for contato in agenda:
+      #      if contato['agenda_id'] == agenda_id:
+       #         return contato
+       # return None
--- resources/agenda.py
+++ resources/agenda.py
@@ -1,45 +1,10 @@
 from flask_restful import Resource, reqparse
 from models.agenda import AgendaModel
-agenda = [
-    {
-        'agenda_id': '1',
-        'nome': 'Jarbas Vasconselos',
-        'numero contato': '88182833',
-        'email': 'teste@gmail.com',
-        'cidade' : 'juazeiro do norte'
-    },
-
-    {
-        'agenda_id': '2',
-        'nome': 'Firmino Mimoso',
-        'numero contato': '88182833',
-        'email': 'teste@gmail.com',
-        'cidade' : 'juazeiro do norte'
-    },
-
-    {
-        'agenda_id': '3',
-        'nome': 'Gabriel do Aliexpress',
-        'numero contato': '88182833',
-        'email': 'teste@gmail.com',
-        'cidade' : 'juazeiro do norte'
-    },
-
-    {
-        'agenda_id': '4',
-        'nome': 'Luan fumador de erva',
-        'numero contato': '88182833',
-        'email': 'teste@gmail.com',
-        'cidade' : 'juazeiro do norte'
-    }
-]
-
-
 
 
 class Agenda(Resource):
     def get(self):
-        return {'agenda': agenda}
+        return {'agenda': [contato.json() for contato in AgendaModel.query.all()]}
 
 class Contato(Resource):
     argumentos = reqparse.RequestParser()
@@ -49,37 +14,45 @@ class Contato(Resource):
     argumentos.add_argument('cidade')
 
 
-    def find_agenda(agenda_id):
-        for contato in agenda:
-            if contato['agenda_id'] == agenda_id:
-                return contato
-        return None
-
     def get(self, agenda_id):
-        contato = Contato.find_agenda(agenda_id)
+        contato = AgendaModel.find_agenda(agenda_id)
         if contato:
-            return contato
+            return contato.json()
         return {'message': 'contato not found'}, 404
 
     def post(self, agenda_id):
+        if AgendaModel.find_agenda(agenda_id):
+            return {"message": "agenda id '{}' already exists.".format(agenda_id)}, 400
+
         dados = Contato.argumentos.parse_args()
-        obj_contato = AgendaModel(agenda_id, **dados)
-        novo_contato = obj_contato.json()
-        agenda.append(novo_contato)
-        return novo_contato, 200
+        contato = AgendaModel(agenda_id, **dados)
+        try:
+            contato.save_agenda()
+        except:
+            return {'message': 'An internal error ocurred trying to save contato'}, 500 # Internal Server Error
+        return contato.json()
 
     def put(self, agenda_id):
         dados = Contato.argumentos.parse_args()
-        obj_contato = AgendaModel(agenda_id, **dados)
-        novo_contato = obj_contato.json()
-        pointer_contato = Contato.find_agenda(agenda_id)
-        if pointer_contato:
-            pointer_contato.update(novo_contato)
-            return novo_contato, 200
-        agenda.append(novo_contato)
-        return novo_contato, 201
+        contato_encontrado = AgendaModel.find_agenda(agenda_id)
+        if contato_encontrado:
+            contato_encontrado.update_contato(**dados)
+            contato_encontrado.save_agenda()
+            return contato_encontrado.json(), 200
+        contato = AgendaModel(agenda_id, **dados)
+        try:
+            contato.save_agenda()
+        except:
+            return {'message': 'An internal error ocurred trying to save contato'}, 500 # Internal Server Error
+        return contato.json(), 201
+
 
     def delete(self, agenda_id):
-        global agenda
-        agenda = [contato for contato in agenda if contato['agenda_id'] != agenda_id]
-        return {'message': 'Contato deleted.'}
+        contato = AgendaModel.find_agenda(agenda_id)
+        if contato:
+            try:
+                contato.delete_contato()
+            except:
+                return {'message': 'An error ocurred trying to delete contato'}, 500
+            return {'message': 'Contato deleted.'}
+        return {'message': 'Contato not found'}, 404
